dafbef46bcd1adfd008cfc704fea869a7d2604e8
don-harris <donald.walter.harris@gmail.com>
2017-09-01 13:28:46 +1200

    please work

--- calculatorjs.js
+++ calculatorjs.js
@@ -1,1 +1,25 @@
-#calcultor
+var box = document.getElementById('display');
+
+function calculatorScreen(x) {
+  box.value+=x;
+  if(x==='c'){
+    box.value="";
+  }
+}
+
+function answer(){
+  x=box.value;
+  x=eval(x);
+  box.value=x;
+}
+
+function backspace() {
+    var value = document.getElementById("d").value;
+    document.getElementById("d").value = value.substr(0, value.length - 1);
+}
+
+function power(){
+  x=box.value;
+  x=eval(x*x);
+  box.value=x;
+}
